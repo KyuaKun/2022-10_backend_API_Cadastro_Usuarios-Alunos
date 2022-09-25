8b7546ca3d027b7c3982beeacd1b4fb29ed75d7d
Vinícius Soares <vinivsp2304@hotmail.com>
2022-09-24 21:34:37 -0300

    add UserController : metodo index => lista todos os usuários do banco de dados

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -1,11 +1,20 @@
-import User from "../models/UserModel";
+import UserModel from "../models/UserModel";
 
 class UserController {
   async store(req, res) {
     try {
       const { email, password } = req.body;
-      const user = await User.create({ email: email, password: password });
-      return res.json({ new_user: user });
+      const user = await UserModel.create({ email: email, password: password });
+      return res.status(201).json({ new_user: user });
+    } catch (err) {
+      return res.status(400).json({ error: err.errors.map((e) => e.message) });
+    }
+  }
+
+  async index(req, res) {
+    try {
+      const user = await UserModel.findAll();
+      return res.status(201).json({ all_users: user });
     } catch (err) {
       return res.status(400).json({ error: err.errors.map((e) => e.message) });
     }
--- src/routes/UserRoutes.js
+++ src/routes/UserRoutes.js
@@ -3,6 +3,7 @@ import UserController from "../controllers/UserController";
 
 const router = new Router();
 
-router.post("/", UserController.store);
+router.post("/store", UserController.store);
+router.get("/index", UserController.index);
 
 export default router;
