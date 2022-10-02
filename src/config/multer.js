2958226b31a7883fc9413652cae77f5e55751732
Vinícius Soares <vinivsp2304@hotmail.com>
2022-10-02 00:52:22 -0300

    Update multer.js
    add: filtro de tipos de arquivo que podem ser enviados.

--- src/config/multer.js
+++ src/config/multer.js
@@ -2,9 +2,23 @@ import multer from "multer";
 import { extname, resolve } from "path";
 
 const aleatorio = () => Math.floor(Math.random() * 10000 + 10000);
-console.log(aleatorio());
 
 export default {
+  fileFilter: (req, file, cb) => {
+    if (
+      file.mimetype != "image/png" &&
+      file.mimetype != "image/jpg" &&
+      file.mimetype != "image/jpeg"
+      //   file.mimetype != "GIF"
+    ) {
+      return cb(
+        new multer.MulterError(
+          "Arquivo deve ser um dos seguintes formatos: png, jpg, jpeg ou gif"
+        )
+      );
+    }
+    return cb(null, true);
+  },
   storage: multer.diskStorage({
     destination: (req, file, cb) => {
       cb(null, resolve("uploads"));
