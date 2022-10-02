ae82e6c9cb5db6da214d136f49ee75f494c192ea
Vinícius Soares <vinivsp2304@hotmail.com>
2022-10-02 00:10:17 -0300

    Update photoRoutes.js
    delete: route.get

--- src/routes/photoRoutes.js
+++ src/routes/photoRoutes.js
@@ -1,8 +1,13 @@
 import { Router } from "express";
+import multer from "multer";
+
 import PhotoController from "../controllers/PhotoController";
+import multerConfig from "../config/multer";
+
+const upload = multer(multerConfig);
 
 const router = new Router();
 
-router.get("/", PhotoController.index);
+router.post("/", upload.single("photo"), PhotoController.store);
 
 export default router;
