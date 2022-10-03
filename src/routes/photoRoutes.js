d221dcd695801ce795304c9eb94e58136be8969e
Vinícius Soares <vinivsp2304@hotmail.com>
2022-10-03 16:43:36 -0300

    Update photoRoutes.js
    add: middleware na rota

--- src/routes/photoRoutes.js
+++ src/routes/photoRoutes.js
@@ -1,9 +1,10 @@
 import { Router } from "express";
 
 import PhotoController from "../controllers/PhotoController";
+import regularUser from "../middlewares/regularUser";
 
 const router = new Router();
 
-router.post("/", PhotoController.store);
+router.post("/", regularUser, PhotoController.store);
 
 export default router;
