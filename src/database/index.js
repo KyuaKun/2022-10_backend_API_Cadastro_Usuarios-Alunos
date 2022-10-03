24422d5d3ddfa9ddc51956752be25124cecd5a50
Vinícius Soares <vinivsp2304@hotmail.com>
2022-10-03 16:45:15 -0300

    Update index.js

--- src/database/index.js
+++ src/database/index.js
@@ -3,8 +3,10 @@ import databaseConfig from "../config/database";
 
 import Aluno from "../models/AlunoModel";
 import User from "../models/UserModel";
+import Photo from "../models/PhotoModel";
 
-const models = [Aluno, User];
+const models = [Aluno, User, Photo];
 const connection = new Sequelize(databaseConfig);
 
 models.forEach((model) => model.init(connection));
+models.forEach((model) => model.associate && model.associate(connection.models));
