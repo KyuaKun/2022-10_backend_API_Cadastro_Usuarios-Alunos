f2d9ca81325db26973faf0c008efaf444a7baaec
Vinícius Soares <vinivsp2304@hotmail.com>
2022-10-03 16:46:19 -0300

    Update PhotoController.js
    add: Metodo Store para salvar as fotos do aluno

--- src/controllers/PhotoController.js
+++ src/controllers/PhotoController.js
@@ -1,16 +1,31 @@
 import multer from "multer";
 import multerConfig from "../config/multer";
+import PhotoModel from "../models/PhotoModel";
 
 const upload = multer(multerConfig).single("photo");
 
 class PhotoController {
-  async store(req, res) {
-    return upload(req, res, (err) => {
+  store(req, res) {
+    return upload(req, res, async (err) => {
       if (err) {
         return res.status(400).json({ error: err.code });
       }
 
-      return res.status(200).json(req.file);
+      const { originalname, filename } = req.file;
+      const { aluno_id } = req.body;
+      try {
+        const photo = await PhotoModel.create({
+          originalname,
+          filename,
+          aluno_id,
+        });
+
+        return res.status(200).json(photo);
+      } catch (msg) {
+        return res.status(400).json({
+          error: "algo inesperado aconteceu",
+        });
+      }
     });
   }
 }
