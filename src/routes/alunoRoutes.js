708c032e432c23ff5e6d1c2abea9d98e159feeb4
Vinícius Soares <vinivsp2304@hotmail.com>
2022-10-05 18:48:29 -0300

    Update alunoRoutes.js

--- src/routes/alunoRoutes.js
+++ src/routes/alunoRoutes.js
@@ -1,12 +1,13 @@
 import { Router } from "express";
 import AlunoController from "../controllers/AlunoController";
+import regularPermission from "../middlewares/regularUser";
 
 const router = new Router();
 
 router.post("/", AlunoController.store);
-router.get("/all", AlunoController.index);
-router.get("/:id", AlunoController.show);
-router.put("/:id", AlunoController.update);
-router.delete("/:id", AlunoController.delete);
+router.get("/all", regularPermission, AlunoController.index);
+router.get("/:id", regularPermission, AlunoController.show);
+router.put("/:id", regularPermission, AlunoController.update);
+router.delete("/:id", regularPermission, AlunoController.delete);
 
 export default router;
